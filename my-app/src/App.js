7e1218f23b2f3f309e1611bc5881cf1ac636faa7
Gohar33 <vangogik@gmail.com>
2022-03-19 22:03:02 +0100

    added footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import UpperPart from "./Components/UpperPart";
 import MiddlePart from "./Components/MiddlePart";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div>
       <UpperPart />
       <MiddlePart />
+      <Footer />
     </div>
   );
 }
